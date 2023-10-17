db6a8c8cbd926a4163bedb1ab7f175d2d46350de
usamah abdul matin <eexvuu@gmail.com>
2023-10-17 14:31:55 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen">
+        <h1 className="text-4xl font-bold">Base Structure</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
