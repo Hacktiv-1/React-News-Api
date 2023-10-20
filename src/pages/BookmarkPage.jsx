83793e9f55ab9ef06e178cb9fbb093b317fb9920
thahaadly <thahaadly@gmail.com>
2023-10-21 00:39:20 +0800

    Commit pesan Anda di sini

--- src/pages/BookmarkPage.jsx
+++ src/pages/BookmarkPage.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-
 function BookmarkPage() {
   const bookmarks = useSelector((state) => state.bookmarks);
 
   return (
-    <div>
-      <h2>Bookmarked News</h2>
+    <div className="BookmarkPage p-6 bg-gray-200 rounded-lg">
+      <h2 className="text-2xl font-bold text-gray-700 mb-4">Bookmarked News</h2>
       {bookmarks.length === 0 ? (
-        <p>No bookmarks yet.</p>
+        <p className="text-lg text-gray-600">No bookmarks yet.</p>
       ) : (
-        <ul>
-          {bookmarks.map((url, index) => (
+        <ul className="list-disc pl-4">
+          {bookmarks.map((bookmark, index) => (
             <li key={index}>
-              <a href={url} target="_blank" rel="noopener noreferrer">
-                Bookmarked News {index + 1}
+              <a
+                href={bookmark}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 hover:underline"
+              >
+                {bookmark}
               </a>
             </li>
           ))}
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bookmarksReducer from './slices/bookmarksSlice';
 
+
+
 const store = configureStore({
   reducer: {
     bookmarks: bookmarksReducer,
+    
   },
 });
+
 
 export default store;
